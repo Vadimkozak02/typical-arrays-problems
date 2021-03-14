4277f150a746b003decb3971b16850c6f07db0cb
Vadim <vadimkozak02@gmail.com>
2021-03-14 19:44:30 +0300

    pass test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+  for (let key in array) {
+  return Math.min(...array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  for (let key in array) {
+  return Math.max(...array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  for (let key in array) {
+  let sum = 0;
+  let count = array.length;
+  for (i = 0; i < count; i++) {
+    sum += array[i];
+  }
+  return sum/count;
+}
+return 0;
 }
